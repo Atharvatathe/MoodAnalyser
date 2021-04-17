ec1b230a70c2864351c682b5d1e0d5fcf0063443
Atharvatathe <atharva.tathe55@gmail.com>
2021-04-17 23:03:45 +0530

    Added : Handling null Exception and return HAPPY

--- test/java/MoodAnalyseTest.java
+++ test/java/MoodAnalyseTest.java
@@ -2,20 +2,28 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 public class MoodAnalyseTest {
-      @Test
-    public void givenMessage_Whensad_shouldReturnSad()  {
+    @Test
+    public void givenMessage_Whensad_shouldReturnSad() {
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in sad Mood");
         String mood = moodAnalyser.analyseMood();
 
         Assertions.assertEquals("SAD", mood);
-        }
+    }
 
     @Test
-    public void givenMessage_WhenAny_shouldReturnHappy()  {
+    public void givenMessage_WhenAny_shouldReturnHappy() {
         MoodAnalyser moodAnalyser = new MoodAnalyser("I am in Any Mood");
         String mood = moodAnalyser.analyseMood();
 
         Assertions.assertEquals("SAD", mood);
     }
 
+    @Test
+    public void givenMessage_WhenNull_shouldReturnHappy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser(null);
+        String mood = moodAnalyser.analyseMood();
+
+        Assertions.assertEquals("HAPPY", mood);
+
     }
+}
