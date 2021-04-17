95b594588751aea99f935651f01ba74bde0be14a
Atharvatathe <atharva.tathe55@gmail.com>
2021-04-17 21:25:53 +0530

    Added : Analyse and respond Happy or sad Mood

---
+++ test/java/MoodAnalyseTest.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MoodAnalyseTest {
+      @Test
+    public void givenMessage_Whensad_shouldReturnSad()  {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("This is sad message");
+
+        Assertions.assertEquals("SAD", mood);
+        }
+
+    }
